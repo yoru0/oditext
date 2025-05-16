8ae5bcbadf5093f057b4cd48f7067f2dc899c847
yoru0 <jordanchandra1@gmail.com>
2025-05-16 21:57:48 +0700

    add

--- react-app/src/Massage.tsx
+++ react-app/src/Massage.tsx
@@ -1,8 +1,7 @@
 function Message() {
-    const name = '';
-    if (name)
-        return <h1>Hello {name}</h1>;
-    return <h1>Hello Stranger</h1>;
+  const name = "";
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello Stranger</h1>;
 }
 
 export default Message;
