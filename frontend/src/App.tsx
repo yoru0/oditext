56fcd950db643441e21badcf3ded66650b92e9f6
yoru0 <jordanchandra1@gmail.com>
2025-05-19 02:02:23 +0700

    finally

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
-import axios from "axios";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
 
 import Home from "./page/Home";
 import History from "./page/History";
 import Navbar from "./components/Navbar";
 
 export default function App() {
-  const fetchAPI = async () => {
-    const response = await axios.get("http://localhost:8080/api/users");
-    console.log(response.data.user);
-  };
+  // const fetchAPI = async () => {
+  //   const response = await axios.get("http://localhost:8080/api/users");
+  //   console.log(response.data.user);
+  // };
 
-  useEffect(() => {
-    fetchAPI();
-  }, []);
+  // useEffect(() => {
+  //   fetchAPI();
+  // }, []);
 
   return (
     <Router>
