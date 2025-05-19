3e7320121a90ace41c50fc6e3dedd8f953471809
yoru0 <jordanchandra1@gmail.com>
2025-05-19 14:12:12 +0700

    p

--- frontend/src/page/Home.tsx
+++ frontend/src/page/Home.tsx
@@ -4,7 +4,7 @@ import Input from "../components/Input";
 
 export default function Homepage() {
   return (
-    <div className="flex min-h-screen bg-gray-200">
+    <div className="flex min-h-screen bg-gray-100">
       <Sidebar />
       <main className="flex-1 m-5 p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12 bg-gray-100 rounded-2xl shadow-xl border border-gray-200 flex items-center justify-center">
         <div className="w-full max-w-3xl -mt-15">
